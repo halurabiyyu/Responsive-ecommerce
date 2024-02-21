0b1595eb7de588ee7cc6eae3bad8d779c984dc5d
halurabiyyu <halurabiyyu@gmail.com>
2024-02-21 20:54:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+// menu show
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
+
+// Close Menu
+const navLink = document.querySelectorAll(".nav__link"),
+  navMenu = document.getElementById("nav-menu");
+
+function linkAction() {
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.scrollY
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*= ' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*= ' + sectionId + ']').classList.remove('active')
+        }
+    })
+    
+}
