5e6370f6f5a6d3b6dbeae6817abf69548b624908
halurabiyyu <halurabiyyu@gmail.com>
2024-02-29 07:32:11 +0700

    feat: home display

--- main.js
+++ main.js
@@ -41,3 +41,9 @@ function scrollActive() {
     })
     
 }
+
+// CHANGE COLOR HEADER 
+window.onscroll = () => {
+  const nav = document.getElementById('header')
+  if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
